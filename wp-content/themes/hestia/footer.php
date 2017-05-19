864f9cef9e4a7754fa5c142e5f4b1f84ace934e4
Claudia <cblancog94@gmail.com>
2017-05-19 14:46:48 -0400

    adding google map iframe

--- wp-content/themes/hestia/footer.php
+++ wp-content/themes/hestia/footer.php
@@ -22,34 +22,38 @@ $hestia_general_credits = get_theme_mod( 'hestia_general_credits',
 			)
 		)
 		); ?>
-		<footer class="footer footer-black footer-big">
+		<footer class="footer footer-black footer-big" id="footer">
+		<br>
 			<div class="container">
 				<div class="content">
-					<div class="row">
-						<div class="col-md-12" align="" style="background: ; width: 700px">
-							<h3><b>Oficinas</b></h3>
-							<hr>
-							<div class="col-md-6" align="left">
-								<div class="col-md-6" align="left">
-									<p>
-										N° 75, Costa Sur,<br> EL Doral
-										<br>Ciudad de Panamá
-										<br><b>Panamá</b>
-										<br><i class="fa fa-phone"></i>&nbsp (0507) 8311172
-									</p> 									
-								</div>
-								<div clas="col-md-6" align="left">
-									<p>
-										C.C. Manzanares Plaza,<br>
-										Piso 1, Oficina 01-04
-										<br>Caracas
-										<br><b>Venezuela</b>
-										<br><i class="fa fa-phone"></i>&nbsp (0058) 2129421048
-									</p>
-								</div>
-							</div>
-						</div>
+					<div class="col-md-12" align="" style="background:">
+						<h3><b>Oficinas</b></h3>
+						<hr>
+							<table>
+								<tr>
+									<td style=" width: 600px">
+										<p>
+											N° 75, Costa Sur,<br> EL Doral
+											<br>Ciudad de Panamá
+											<br><a href="https://goo.gl/maps/UUhRLZM8Qmt"><i class="fa fa-map-marker"></i>&nbsp<b>Panamá</b></a>
+											<br><i class="fa fa-phone"></i>&nbsp +507 8311172
+										</p> 
+										<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7880.706853012552!2d-79.43015752805319!3d9.031488324076491!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8facaa808ae67af9%3A0x12324cbbd6fbd4b8!2sPlaza+Costa+Sur%2C+Panama+City%2C+Panama!5e0!3m2!1sen!2sve!4v1495218344997" width="600"></iframe>		
+									</td>
+									<td style=" width: 600px">
+										<p>
+											C.C. Manzanares Plaza,<br>
+											Piso 1, Oficina 01-04
+											<br>Caracas
+											<br><a href="https://goo.gl/maps/EZA7xeWNryH2"><i class="fa fa-map-marker"></i>&nbsp<b>Venezuela</b></a>
+											<br><i class="fa fa-phone"></i>&nbsp +58 2129421048
+										</p>
+										<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3923.204315708614!2d-66.85643478577688!3d10.484553692519958!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c2af614c2690efd%3A0x4a82b48117eddf38!2sCentro+Comercial+Manzanares+Plaza!5e0!3m2!1sen!2sve!4v1495218728436" width="600"></iframe>	
+									</td>
+								</tr>
+							</table>
 					</div>
+
 				</div>
 				<div>
 					<h2>
