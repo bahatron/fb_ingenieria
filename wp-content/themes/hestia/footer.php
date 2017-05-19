d2637df710f2578c5323172c1a957488cb869dc4
Claudia <cblancog94@gmail.com>
2017-05-19 12:22:57 -0400

    adding addresses and contact info

--- wp-content/themes/hestia/footer.php
+++ wp-content/themes/hestia/footer.php
@@ -15,31 +15,57 @@ $hestia_general_credits = get_theme_mod( 'hestia_general_credits',
 		/* translators: %s is Theme name */
 		sprintf( '<a href="https://themeisle.com/themes/hestia/" target="_blank" rel="nofollow">%s</a>',
 			esc_html__( 'Hestia', 'hestia' )
-		),
+			),
 		/* translators: %s is WordPress */
-	    sprintf( '<a href="http://wordpress.org/" rel="nofollow">%s</a>',
+		sprintf( '<a href="http://wordpress.org/" rel="nofollow">%s</a>',
 			esc_html__( 'WordPress', 'hestia' )
+			)
 		)
-	)
-); ?>
-				<footer class="footer footer-black footer-big">
-					<div class="container">
-						<div class="content">
-							<div class="row">
-								<div class="col-md-12" align="center">
-									<h2>
-										<a href="https://twitter.com/FBIngenieria"><i class="fa fa-twitter"></i></a>
-										<a href="https://www.instagram.com/fbingenieriayproyectos/"><i class="fa fa-instagram"></i></a>
-									</h2>
+		); ?>
+		<footer class="footer footer-black footer-big">
+			<div class="container">
+				<div class="content">
+					<div class="row">
+						<div class="col-md-12" align="">
+						<h3><b>Oficinas</b></h3>
+							<div class="col-md-6" align="left">
+								<div class="col-md-6" align="left">
+									<p>
+										N° 75, Costa Sur,<br> EL Doral
+										<br>Ciudad de Panamá
+										<br><b>Panamá</b>
+										<br><i class="fa fa-phone"></i>&nbsp (0507) 8311172
+									</p> 									
+								</div>
+								<div clas="col-md-6" align="left">
+									<p>
+										C.C. Manzanares Plaza,<br>
+										Piso 1, Oficina 01-04
+										<br>Caracas
+										<br><b>Venezuela</b>
+										<br><i class="fa fa-phone"></i>&nbsp (0058) 2129421048
+									</p>
 								</div>
 							</div>
 						</div>
-						<hr>
-						<p>2017 © Copyrights FB Ingeniería</p>
 					</div>
-				</footer>
-		</div>
+				</div>
+				<div>
+					<h2>
+						<a href="https://twitter.com/FBIngenieria"><i class="fa fa-twitter"></i></a>
+						<a href="https://www.instagram.com/fbingenieriayproyectos/"><i class="fa fa-instagram"></i></a>
+
+					</h2>
+					<p>
+						<br><i class="fa fa-envelope"></i>&nbsp fbingenieriaproyectos@fbingenieria.com
+					</p>
+				</div>
+				<hr>
+				<p>2017 © Copyrights FB Ingeniería</p>
+			</div>
+		</footer>
 	</div>
+</div>
 <?php wp_footer(); ?>
 </body>
 </html>
