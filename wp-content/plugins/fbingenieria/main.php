99f15b3a1363a6db4aff3aeb2b2aee902e32bed3
Simon Piscitelli <simonpiscitelli@gmail.com>
2017-05-18 21:29:13 -0400

    landing_page shortcode injector

---
+++ wp-content/plugins/fbingenieria/main.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Plugin Name: FBIngenieria
+Version: 1.0.0
+Author: Claudia & Simon Piscitelli
+Description: FBIngenieria pages
+Text Domain: fbingenieria
+*/
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class FBIngenieria
+{
+    public function __construct()
+    {
+        define('FBINGENIERIA_PATH', plugin_dir_path(__FILE__));
+        define('FBINGENIERIA_URL', plugins_url(basename(plugin_dir_path(__FILE__)), basename(__FILE__)));
+        $this->load_wp_functions();
+    }
+
+    public function load_wp_functions()
+    {
+        load_plugin_textdomain('fbingenieria', false, plugin_basename(dirname(__FILE__)) . '/languages');
+        add_shortcode('fbi_landing_page', 'fbi_landing_page_handler');
+    }
+}
+
+$GLOBALS['FBIngenieria'] = new FBIngenieria();
+
+function fbi_landing_page_handler($atts)
+{
+    //ob_start();
+    include FBINGENIERIA_PATH.'src/landing_page.html';
+    //return ob_get_clean();
+}
