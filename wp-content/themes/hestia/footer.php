8e1082f0bd641562ab4356b5542be171426a842a
Claudia <cblancog94@gmail.com>
2017-06-16 19:58:48 -0400

    working on footer css

--- wp-content/themes/hestia/footer.php
+++ wp-content/themes/hestia/footer.php
@@ -25,87 +25,102 @@ $hestia_general_credits = get_theme_mod('hestia_general_credits',
 	<?php
 		global $FBIngenieria;
 	?>
-	<footer id="fbi_footer" class="footer footer-big" data-app>
-		<div class="row" style="width: 100%;">
-			<div class="col-md-12">
-				<div style="height: 100px"></div>
-				<div class="office-section" align="left">
-					<p class="p-office-section">Oficinas</p>
-					<hr class="hr-color">
-					<table>
-						<tr>
-							<td style=" width: auto;">
-								<p>
-									Corregimiento Juan Díaz,<br>
-									N° 75, Costa Sur,<br> El Doral
-									<br>Ciudad de Panamá
-									<br><a target="_blank" href="https://goo.gl/maps/UUhRLZM8Qmt"><i class="fa fa-map-marker"></i>&nbsp<b>Panamá</b></a>
-									<br><i class="fa fa-phone"></i>&nbsp +507 8311172
-								</p>
-							</td>
-							<td style=" width: auto;">
-								<p>
-									Av. Principal de Manzanares,<br>
-									C.C. Manzanares Plaza,<br> Piso 1, Oficina 01-04
-									<br>Caracas
-									<br><a target="_blank" href="https://goo.gl/maps/EZA7xeWNryH2"><i class="fa fa-map-marker"></i>&nbsp<b>Venezuela</b></a>
-									<br><i class="fa fa-phone"></i>&nbsp +58 2129421048
-								</p>
-							</td>
-						</tr>
-					</table>
-					<div align="left" style="margin-top: -30px;">
-						<h4>
-							<a target="_blank" class="footer-a" href="https://www.instagram.com/fbingenieriayproyectos/"><i class="fa fa-instagram"></i></a>
-							<a target="_blank" class="footer-a" href="https://www.linkedin.com/company-beta/5901993/"><i class="fa fa-linkedin-square"></i></a>
-							<a><i class="fa fa-envelope-square"></i></a>
-						</h4>
+		<footer id="fbi_footer" class="footer footer-big" data-app>
+			<div class="row" style="width: 100%;">
+				<div class="col-md-12">
+					<div style="height: 100px"></div>
+					<div class="office-section" align="left">
+						<p class="p-office-section">Oficinas</p>
+						<hr class="hr-color">
+						<table>
+							<tr>
+								<td style=" width: auto;">
+									<p>
+										Corregimiento Juan Díaz,<br> N° 75, Costa Sur,<br> El Doral
+										<br>Ciudad de Panamá
+										<br><a target="_blank" href="https://goo.gl/maps/UUhRLZM8Qmt"><i class="fa fa-map-marker"></i>&nbsp<b>Panamá</b></a>
+										<br><i class="fa fa-phone"></i>&nbsp +507 8311172
+									</p>
+								</td>
+								<td style=" width: auto;">
+									<p>
+										Av. Principal de Manzanares,<br> C.C. Manzanares Plaza,<br> Piso 1, Oficina 01-04
+										<br>Caracas
+										<br><a target="_blank" href="https://goo.gl/maps/EZA7xeWNryH2"><i class="fa fa-map-marker"></i>&nbsp<b>Venezuela</b></a>
+										<br><i class="fa fa-phone"></i>&nbsp +58 2129421048
+									</p>
+								</td>
+							</tr>
+						</table>
+						<div align="left" style="margin-top: -30px;">
+							<h4>
+								<a target="_blank" class="footer-a" href="https://www.instagram.com/fbingenieriayproyectos/"><i class="fa fa-instagram"></i></a>
+								<a target="_blank" class="footer-a" href="https://www.linkedin.com/company-beta/5901993/"><i class="fa fa-linkedin-square"></i></a>
+								<a><i class="fa fa-envelope-square"></i></a>
+								<template>
+									<v-layout row justify-center>
+										<v-dialog v-model="dialog">
+											<v-btn primary light slot="activator">Open Dialog</v-btn>
+											<v-card>
+												<v-card-row>
+													<v-card-title>Use Google's location service?</v-card-title>
+												</v-card-row>
+												<v-card-row>
+													<v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no
+														apps are running.</v-card-text>
+												</v-card-row>
+												<v-card-row actions>
+													<v-btn class="green--text darken-1" flat="flat" @click.native="dialog = false">Disagree</v-btn>
+													<v-btn class="green--text darken-1" flat="flat" @click.native="dialog = false">Agree</v-btn>
+												</v-card-row>
+											</v-card>
+										</v-dialog>
+									</v-layout>
+								</template>
+
+							</h4>
+						</div>
 					</div>
 				</div>
-			</div>
-			<div class="col-md-12">
-				<div style="height: 100px"></div>
-			</div>
-			<div class="col-md-12" style="height: 100%; width:100%; background:#202835; border-top-style: solid; border-top-color: #fb6816; padding-top: 10px; "
-			    align="center">
-				<p>2017 © Copyrights FB Ingeniería</p>
-				<a href="#home"><?php echo $FBIngenieria->translate('back-to-top', $lang) ?></a>
-				<form action="" method="GET" id="changeLanguageForm">
-					<img style="max-height: 30px; cursor: pointer;" src="<?php echo FBINGENIERIA_URL.'/src/assets/img/1495683664_United-States-Flag.png' ?>"
-					    onclick="changeLanguage('en')">
-					<img style="max-height: 30px; cursor: pointer;" src="<?php echo FBINGENIERIA_URL.'/src/assets/img/1495683661_Spain-Flag.png' ?>"
-					    onclick="changeLanguage('es')">
-					<input type="hidden" name="lang" value="" style="display: none;">
-				</form>
-			</div>
-		</div>
-		<div class="modal fade" id="myModal" role="dialog">
-			<div class="modal-dialog">
-				<div class="modal-content">
-					<div class="modal-body">
-						<button type="button" class="close" data-dismiss="modal">&times;</button>
-						<p>Escríbenos a <a>fbingenieriayproyectos@fbingenieria.com</a> !</p>
-					</div>
+				<div class="col-md-12">
+					<div style="height: 100px"></div>
+				</div>
+				<div class="col-md-12 cp-footer" align="center">
+					<p>2017 © Copyrights FB Ingeniería</p>
+					<a href="#home"><?php echo $FBIngenieria->translate('back-to-top', $lang) ?></a>
+					<form action="" method="GET" id="changeLanguageForm">
+						<img style="max-height: 30px; cursor: pointer;" src="<?php echo FBINGENIERIA_URL.'/src/assets/img/1495683664_United-States-Flag.png' ?>"
+						    onclick="changeLanguage('en')">
+						<img style="max-height: 30px; cursor: pointer;" src="<?php echo FBINGENIERIA_URL.'/src/assets/img/1495683661_Spain-Flag.png' ?>"
+						    onclick="changeLanguage('es')">
+						<input type="hidden" name="lang" value="" style="display: none;">
+					</form>
 				</div>
 			</div>
-		</div>
-		</div>
-	</footer>
-	<?php wp_footer(); ?>
-	</body>
 
-	</html>
-	<script type="text/javascript">
-		function mail() {
-			document.getElementById('prueba').innerHTML = '<v-alert info v-bind:value="true">This is a info alert.</v-alert>';
-		}
+		</footer>
+		<?php wp_footer(); ?>
+		</body>
 
-		function changeLanguage(lang) {
-			var form = document.getElementById('changeLanguageForm');
-			form.elements.lang.value = lang;
-			form.submit();
-		}
-		new Vue({
-			el: '#fbi_footer'
-		})
-	</script>
+		</html>
+		<script type="text/javascript">
+			function mail() {
+				document.getElementById('prueba').innerHTML = '<v-alert info v-bind:value="true">This is a info alert.</v-alert>';
+			}
+
+			function changeLanguage(lang) {
+				var form = document.getElementById('changeLanguageForm');
+				form.elements.lang.value = lang;
+				form.submit();
+			}
+			new Vue({
+				el: '#fbi_footer'
+			})
+			export default {
+				data() {
+					return {
+						dialog: false
+					}
+				}
+			}
+		</script>
