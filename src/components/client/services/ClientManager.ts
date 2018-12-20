0401f980fda1c99eacabb4e394ee916804ff65cc
Simon Piscitelli <simonpiscitelli@gmail.com>
2018-12-20 15:32:40 +0000

    Add closure factory

--- src/components/client/services/ClientManager.ts
+++ src/components/client/services/ClientManager.ts
@@ -1,7 +1,6 @@
 import $firebase from "../../../services/firebase";
 import { Client, ClientData } from "../ClientFacade";
 import $clientValidator from "./ClientValidator";
-
 const $db = $firebase.database();
 const uuid = require("uuid");
 
@@ -10,6 +9,36 @@ const BASE_PATH = process.env.VUE_APP_DB_CLIENT_PATH;
 interface ClientInstance {
     id: string;
     ref: firebase.database.Reference;
+}
+
+/**
+ * @todo: measure impact of this function vs protoFactory
+ */
+function closureFactory(reference: firebase.database.Reference): Client {
+    return {
+        id: <string>reference.key,
+        
+        async update(data: any): Promise<ClientData> {
+            const clientData = $clientValidator.validate(data);
+            await reference.update(clientData);
+
+            return clientData;
+        },
+
+        async data(): Promise<ClientData> {
+            const data = await reference.once("value");
+
+            return data.val();
+        },
+
+        on(condition: string, callback: (data: ClientData) => void): void {
+            reference.on(<firebase.database.EventType>condition, snapshot => {
+                if (snapshot) {
+                    callback(snapshot.val());
+                }
+            });
+        },
+    };
 }
 
 // Client object prototype
@@ -36,7 +65,7 @@ const $proto = Object.freeze({
     },
 });
 
-function factory(reference: firebase.database.Reference): Client {
+function protoFactory(reference: firebase.database.Reference): Client {
     const instance: ClientInstance = {
         id: <string>reference.key,
         ref: reference,
@@ -52,7 +81,7 @@ const $clientManager = Object.freeze({
 
         await reference.set($clientValidator.validate(data));
 
-        return factory(reference);
+        return protoFactory(reference);
     },
 
     async all(): Promise<Client[]> {
@@ -64,7 +93,7 @@ const $clientManager = Object.freeze({
         const collection: Client[] = [];
 
         data.forEach(record => {
-            collection.push(factory(record.ref));
+            collection.push(protoFactory(record.ref));
         });
 
         return collection;
