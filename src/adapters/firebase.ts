72c3ae9dbed97aecc3d390ad4424174f16b224ba
Simon Piscitelli <simonpiscitelli@gmail.com>
2019-03-29 16:21:06 +0000

    Create bucket service skeleton

--- src/adapters/firebase.ts
+++ src/adapters/firebase.ts
@@ -6,6 +6,7 @@ const config = {
     authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
     projectId: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
 };
 
 $firebase.initializeApp(config);
---
+++ src/services/Bucket.ts
@@ -0,0 +1,7 @@
+import $firebase from "../adapters/firebase";
+
+const STORAGE = $firebase.storage();
+
+const $bucket = {};
+
+export default $bucket;
